af9c531a07ced283534230c363f2261509d078f8
Nicede <2262519065@qq.com>
2022-09-04 14:41:20 +0800

    update platform_testing.sh

--- platform_testing.sh
+++ platform_testing.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 architecture=$(arch)
 if [[ ! -d ./tool_bin ]] ;then
-  case $architecture in
-    "x86_64")
+     case $architecture in
+                "x86_64")
               git clone https://github.com/XPHNature/Tool_bin -b Pc
                chmod -R 777 ./Tool_bin/Pc_Bin.7z
                7z x ./Tool_bin/Pc_Bin.7z 
                rm -rf ./Tool_bin
-           ;;
-    "aarch64") 
+            ;;
+                "aarch64") 
                git clone https://github.com/XPHNature/Tool_bin -b Phone
                chmod -R 777 ./Tool_bin/Phone_bin.7z
                7z x ./Tool_bin/Phone_bin.7z
                rm -rf ./Tool_bin
-  esac
+       esac
 fi
