9d12fc76d3e8fc356f540b7cfa18a54727b1f62d
Nicede <2262519065@qq.com>
2022-09-11 10:55:11 +0800

    platform_testing.sh:newly added x86 platform tool_bin

--- platform_testing.sh
+++ platform_testing.sh
@@ -2,16 +2,19 @@
 architecture=$(arch)
 if [[ ! -d ./tool_bin ]] ;then
      case $architecture in
-                "x86_64")
+                x86_64|x86)
               git clone https://github.com/XPHNature/Tool_bin -b Pc
                chmod -R 777 ./Tool_bin/Pc_Bin.7z
                7z x ./Tool_bin/Pc_Bin.7z 
                rm -rf ./Tool_bin
             ;;
-                "aarch64") 
+                aarch64) 
                git clone https://github.com/XPHNature/Tool_bin -b Phone
                chmod -R 777 ./Tool_bin/Phone_bin.7z
                7z x ./Tool_bin/Phone_bin.7z
                rm -rf ./Tool_bin
+            ;;
+            *)
+            echo "114514"
        esac
 fi
