fd26e044e70f54737af40657fff18e49afc1379f
Nicede <2262519065@qq.com>
2022-09-23 22:12:27 +0800

    unpackrom.sh:fix UnpackPayload mv error

--- unpackrom.sh
+++ unpackrom.sh
@@ -3,8 +3,7 @@ function UnpackPayload(){
 	mv ./payload.bin ../payload
     echo "解压payload.bin中..."
     cd ../payload
-    python3 ./payload.py ./payload.bin ./out
-    mv ./payload.bin ../temp
+    python3 ./payload.py ./payload.bin ./temp
     echo "解压成功移动至temp目录..."
     cd ../temp
 }
