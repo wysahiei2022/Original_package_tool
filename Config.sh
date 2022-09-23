d4cf6263bda1382baa4aaf14aabee13cb09309b2
Nicede <2262519065@qq.com>
2022-09-24 03:27:44 +0800

    Config.sh:Optimize comments and add marge

--- Config.sh
+++ Config.sh
@@ -1,5 +1,12 @@
-nfc=y #一键删除NFC
-light=y #修复亮度
-phh=y #PHH化
-fixbug=y #修复BUG
-fix=flyme #修复BUG的系统（小写）和上面关联在一起的（fixbug)
+#一键删除NFC（y/n）
+nfc=y
+#修复亮度（y/n）
+light=y
+#PHH化（y/n）
+phh=y
+#修复BUG （y/n）
+fixbug=y
+#修复BUG的系统（小写）和上面关联在一起的（fixbug)(小写)（支持miui flyme ==）
+fix=flyme
+#合并到内外层 内：/system/system/system_ext 外：/system/system_ext 我建议外层 外层用法："等号后面加out，OUT，Out 内层用法：Lnside，lnside
+Merge=out 
