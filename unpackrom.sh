1ab014ae5779bc78e1d2b02ffc3dea406b88d2af
Nicede <2262519065@qq.com>
2022-09-23 21:40:10 +0800

    unpackrom.sh:add unpayload

---
+++ unpackrom.sh
@@ -0,0 +1,10 @@
+#!/system/bin/bash
+function UnPayload(){
+	mv ./payload.bin ../payload
+    echo "解压payload.bin中..."
+    cd ../payload
+    python3 ./payload.py ./payload.bin ./out
+    mv ./payload.bin ../temp
+    echo "解压成功移动至temp目录..."
+    cd ../temp
+}
