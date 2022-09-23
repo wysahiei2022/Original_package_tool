294eceba94a5743653dbd74c86a4dfffd54d0595
Nicede <2262519065@qq.com>
2022-09-23 22:09:10 +0800

    unpackrom.sh:add UnpackBr

--- unpackrom.sh
+++ unpackrom.sh
@@ -8,3 +8,9 @@ function UnpackPayload(){
     echo "解压成功移动至temp目录..."
     cd ../temp
 }
+function UnpackBr(){
+    for brfile in $( find ./tmp -name "**.br" ) ; do
+    echo "正在解压$brfile"
+    brotli -d ./$brfile ../temp
+    done
+}
