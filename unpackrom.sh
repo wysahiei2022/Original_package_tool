075f15297026ec635196fe9d9644974ee7bd1bbb
Nicede <2262519065@qq.com>
2022-09-23 22:36:04 +0800

    unpackrom.sh:add UnpackDat

--- unpackrom.sh
+++ unpackrom.sh
@@ -13,3 +13,29 @@ function UnpackBr(){
     brotli -d ./$brfile ../temp
     done
 }
+function UnpackDat(){
+    if [[ -e system.new.dat ]] ;then
+        echo "正在解压system.new.dat"
+        python3 $bin/sdat2img.py system.transfer.list system.new.bar ./temp/system.img
+    fi
+
+    if [[ -e vendor.new.dat ]] ;then
+        echo "正在解压vendor.new.dat"
+        python3 $bin/sdat2img.py vendor.transfer.list vendor.new.bar ./temp/vendor.img
+    fi
+
+    if [[ -e product.new.dat ]] ;then
+        echo "正在解压product.new.dat"
+        python3 $bin/sdat2img.py product.transfer.list product.new.bar ./temp/product.img
+    fi
+
+    if [[ -e system_ext.new.dat ]] ;then
+        echo "正在解压system.new.dat"
+        python3 $bin/sdat2img.py system_ext.transfer.list system_ext.new.bar ./temp/system_ext.img
+    fi
+
+    if [[ -e odm.new.dat ]] ;then
+        echo "正在解压odm.new.dat"
+        python3 $bin/sdat2img.py odm.transfer.list odm.new.bar ./temp/odm.img
+    fi
+}
