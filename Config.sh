9f72e6709ff384faba1032d4fab8d4fb49543f97
XLPWHZ <68060762+XPHXPHL@users.noreply.github.com>
2022-11-20 11:09:34 +0800

    Update Config.sh

--- Config.sh
+++ Config.sh
@@ -8,5 +8,3 @@ phh=y
 fixbug=y
 #修复BUG的系统（小写）和上面关联在一起的（fixbug)(小写)（支持miui flyme ==）
 fix=flyme
-#合并到内外层 内：/system/system/system_ext 外：/system/system_ext 我建议外层 外层用法："等号后面加out，OUT，Out 内层用法：Lnside，lnside
-Merge=out 
